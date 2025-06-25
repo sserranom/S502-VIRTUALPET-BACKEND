ebcee99f22436e49d9c4f388b83fd2bab69560b4
sserranom <sergiomelgara@gmail.com>
2025-06-25 18:03:28 +0200

    First

---
+++ src/main/java/S502/VIRTUALPETS/VirtualpetsApplication.java
@@ -0,0 +1,13 @@
+package S502.VIRTUALPETS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualpetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualpetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/S502/VIRTUALPETS/VirtualpetsApplicationTests.java
@@ -0,0 +1,13 @@
+package S502.VIRTUALPETS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualpetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
